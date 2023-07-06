f085e7e9120185dd89d802fc6a1e51475d9c0111
Lucas <88401857+Lucas-ELDS@users.noreply.github.com>
2023-07-06 01:06:28 -0300

    Update servidor.py

--- JogoDaVelha/servidor.py
+++ JogoDaVelha/servidor.py
@@ -13,7 +13,8 @@ class ServidorDoJogo:
     self.udp.bind(self.local)
     print('Servidor está no ar...')
     self.recebimento_de_msgs()
-  
+
+  # Gerencia as mensagens recebidas
   def recebimento_de_msgs(self):
     self.lock = threading.Lock()
     self.jogadores = []
@@ -33,7 +34,7 @@ class ServidorDoJogo:
           break
       self.lock.release()
 
-
+  # Verifica a quantidade de jogadores que estão na sala
   def verificar_sala(self, cliente):
     if len(self.jogadores) < 2:
       self.jogadores.append(cliente)
@@ -47,6 +48,7 @@ class ServidorDoJogo:
       resposta = 'Full/A Sala está Cheia.'
       self.udp.sendto(resposta.encode(), cliente)
 
+  # Inicia a partida
   def iniciar_jogo(self):
     self.tab = tabuleiro.Tabuleiro()
     self.jogador_da_vez = 0
@@ -55,7 +57,8 @@ class ServidorDoJogo:
     self.thread_enviar(self.comecar_jogo, 'Start/Jogo Comecou!!')
     self.thread_enviar(self.mandar_representacao, self.tab)
     self.jogue()
-        
+
+  # Faz o jogo rodar enviando comandos para o jogador
   def jogo_rodando(self, cliente, msg):
     if cliente == self.jogadores[self.jogador_da_vez]:
       if msg[0] == 'Insert':
@@ -84,19 +87,21 @@ class ServidorDoJogo:
           self.jogue()
 
 #--------------------------------------------------------
-  
+
   def comecar_jogo(self, i, resposta):
     self.udp.sendto(resposta.encode(), self.jogadores[i])
 
+  # Representação do tabuleiro
   def mandar_representacao(self, i, tabuleiro):
     resposta = 'Content/' + tabuleiro.repr()
     self.udp.sendto(resposta.encode(), self.jogadores[i])
 
+  # Turno do jogador
   def jogue(self):
-    #sua vez
     resposta = 'Insert/' + f'Turno do {self.turno}, qual sua jogada?'
     self.udp.sendto(resposta.encode(), self.jogadores[self.jogador_da_vez])
 
+  # Checagem de jogada válida
   def jogada_atual(self, jogada):
     if jogada in ('1', '2', '3', '4', '5', '6', '7', '8', '9'):
       if self.verificar_jogada(jogada):
@@ -112,12 +117,14 @@ class ServidorDoJogo:
       self.udp.sendto(resposta.encode(), self.jogadores[self.jogador_da_vez])
       return False
 
+  # Verifica se jogou em um espaço vazio
   def verificar_jogada(self, jogada):
     if self.tab.tabuleiro[int(jogada)-1] == ' ':
       return True
     else:
       return False
 
+  # Muda o turno a cada jogada
   def mudar_turno(self):
     if self.turno == 'X':
       self.turno = 'O'
@@ -126,20 +133,24 @@ class ServidorDoJogo:
       self.turno = 'X'
       self.jogador_da_vez = 0
 
+  # Informa que alguém venceu
   def alguem_venceu(self, jogador, result):
     resposta = (f'Win/{result} é o vencedor')
     self.udp.sendto(resposta.encode(), self.jogadores[jogador])
 
+  # Informa que a partida empatou
   def empate(self, jogador, result):
     resposta = result
     self.udp.sendto(resposta.encode(), self.jogadores[jogador])
 
+  # Faz o envio das mensagens para os jogadores
   def thread_enviar(self, funcao, result):
     thr1 = threading.Thread(target=funcao, args=[0, result])
     thr2 = threading.Thread(target=funcao, args=[1, result])
     thr1.start(), thr2.start()
     thr1.join(), thr2.join()
-    
+
+  # Verifica se alguém venceu checando vertical, horizontal e diagonal
   def verificar_tabuleiro(self):
     global result_v
     result_v = ' '
@@ -155,7 +166,8 @@ class ServidorDoJogo:
         return 'empate'
       else:
         return self.tab.tabuleiro.count(' ')
-    
+
+  # Verifica se alguém venceu na vertical
   def verificar_vertical(self):
     global result_v
     if self.tab.tabuleiro[0] != ' ' and self.tab.tabuleiro[0] == self.tab.tabuleiro[3] and self.tab.tabuleiro[3] == self.tab.tabuleiro[6]:
@@ -165,6 +177,7 @@ class ServidorDoJogo:
     elif self.tab.tabuleiro[2] != ' ' and self.tab.tabuleiro[2] == self.tab.tabuleiro[5] and self.tab.tabuleiro[5] == self.tab.tabuleiro[8]:
       result_v = self.tab.tabuleiro[2]
 
+  # Verifica se alguém venceu na horizontal
   def verificar_horizontal(self):
     global result_v
     if self.tab.tabuleiro[0] != ' ' and self.tab.tabuleiro[0] == self.tab.tabuleiro[1] and self.tab.tabuleiro[1] == self.tab.tabuleiro[2]:
@@ -174,6 +187,7 @@ class ServidorDoJogo:
     elif self.tab.tabuleiro[6] != ' ' and self.tab.tabuleiro[6] == self.tab.tabuleiro[7] and self.tab.tabuleiro[7] == self.tab.tabuleiro[8]:
       result_v = self.tab.tabuleiro[6]
 
+  # Verifica se alguém venceu na diagonal
   def verificar_diagonal(self):
     global result_v
     if self.tab.tabuleiro[0] != ' ' and self.tab.tabuleiro[0] == self.tab.tabuleiro[4] and self.tab.tabuleiro[4] == self.tab.tabuleiro[8]:
